cb669689a56ba3aec0edde32082afd44e774feb4
github username <github email address>
2017-08-01 13:42:13 -0400

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,3 @@
-
-<<<<<<< HEAD
-Food.create(food_type: "starches")
-Food.create(food_type: "vegetables")
-Food.create(food_type: "roasted vegetables")
-Food.create(food_type: "soft cheese")
-Food.create(food_type: "hard cheese")
-Food.create(food_type: "fish")
-Food.create(food_type: "rich fish")
-Food.create(food_type: "white meat")
-Food.create(food_type: "red meat")
-Food.create(food_type: "cured meats")
-Food.create(food_type: "sweets")
-=======
 Food.find_or_create_by(food_type: "vegetables")
 Food.find_or_create_by(food_type: "roasted vegetables")
 Food.find_or_create_by(food_type: "soft cheese")
